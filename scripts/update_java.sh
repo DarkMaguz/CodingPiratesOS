154968a3aa93b6d83834c7a73b7f6db1ba69db1c
Peter Balling <peter@knowmio.com>
2018-10-30 01:19:53 +0100

    Ported update_java.sh to be POSIX-compliant so that it works with dash

--- scripts/update_java.sh
+++ scripts/update_java.sh
@@ -1,55 +1,68 @@
-#!/bin/bash
-set -e
-JWM_PATH="/usr/lib/jvm"
-DEFAULT_PATH="/usr/lib/jvm/default-java"
+#!/bin/sh -e
+
+JWM_PATH=/usr/lib/jvm
+DEFAULT_PATH=/usr/lib/jvm/default-java
 
 # Ensure that we have superpowers.
 if [ $(id -u) != 0 ]; then
-        echo "Please run as root"
-        exit
+	echo "Please run as root"
+	exit
 fi
 
-if [ ! -d "$JWM_PATH" ]; then
-	echo "The directory does not exist: $DEFAULT_PATH"
+if [ ! -d $JWM_PATH ]; then
+	echo "The directory does not exist: $JWM_PATH"
 	echo "Terminating..."
 	exit
 fi
 
-cd "$JWM_PATH"
+cd $JWM_PATH
 
-DL_LINK=`wget -q -O - https://java.com/en/download/linux_manual.jsp | xmllint --html --xpath 'string(/html/body/div[2]/div[1]/div/table[2]/tbody/tr[5]/td[2]/a/@href)' - 2>/dev/null`
-HTML_DATA=`wget -q -O - https://java.com/en/download/linux_manual.jsp | xmllint --html --xpath 'string(/html/body/div/div/div/h4[@class="sub"])' - 2>/dev/null`
+apt update -y
+apt install -y libxml2-utils
 
-LATEST_VERSION=`cut -d' ' -f3 <<<$HTML_DATA`
-LATEST_UPDATE=`cut -d' ' -f5 <<<$HTML_DATA`
+DL_LINK=$(wget -q -O - https://java.com/en/download/linux_manual.jsp | xmllint --html --xpath 'string(/html/body/div[2]/div[1]/div/table[2]/tbody/tr[5]/td[2]/a/@href)' - 2> /dev/null)
+HTML_DATA=$(wget -q -O - https://java.com/en/download/linux_manual.jsp | xmllint --html --xpath 'string(/html/body/div/div/div/h4[@class="sub"])' - 2> /dev/null)
+
+LATEST_VERSION=$(echo $HTML_DATA | cut -d' ' -f3)
+LATEST_UPDATE=$(echo $HTML_DATA | cut -d' ' -f5)
 
 CURRENT_VERSION=0
 CURRENT_UPDATE=0
 
-JRE_DIRS=(`ls -d jre*`)
-JRE_DIR=${JRE_DIRS[-1]}
-VERSION_STRING=${JRE_DIR:3}
+LATEST_JRE_DIR=""
+for entry in ./jre*
+do
+	if [ -d $entry ]; then
+		LATEST_JRE_DIR=$entry
+	fi
+done
 
-CURRENT_VERSION=`cut -d'.' -f2 <<<$VERSION_STRING`
-CURRENT_UPDATE=`cut -d'_' -f2 <<<$VERSION_STRING`
+CURRENT_VERSION=$(echo $LATEST_JRE_DIR | cut -d'.' -f3)
+CURRENT_UPDATE=$(echo $LATEST_JRE_DIR | cut -d'_' -f2)
 
-CURRENT_DIR="jre1."$CURRENT_VERSION".0_"$CURRENT_UPDATE""
-LATEST_DIR="jre1."$LATEST_VERSION".0_"$LATEST_UPDATE""
+CURRENT_DIR="jre1.$CURRENT_VERSION.0_$CURRENT_UPDATE"
+LATEST_DIR="jre1.$LATEST_VERSION.0_$LATEST_UPDATE"
 
 if [ "$LATEST_VERSION" != "$CURRENT_VERSION" -o "$LATEST_UPDATE" != "$CURRENT_UPDATE" ]; then
 	echo "Found outdated version of Java JRE: Version $CURRENT_VERSION Update $CURRENT_UPDATE."
 	echo "Downloading new version of Java JRE: Version $LATEST_VERSION Update $LATEST_UPDATE."
 	JRE_ARCHIVE="jre-"$LATEST_VERSION"u"$LATEST_UPDATE"-linux-x64.tar.gz"
-	wget -q --show-progress $DL_LINK -O $JRE_ARCHIVE
+  wget -q --show-progress $DL_LINK -O $JRE_ARCHIVE
 	tar zxvf $JRE_ARCHIVE
 	rm $JRE_ARCHIVE
 	rm -rf $CURRENT_DIR
+	if [ -e $DEFAULT_PATH ]; then
+		rm -f $DEFAULT_PATH
+	fi
+	echo "Creating symbolic link..."
+	ln -s $JWM_PATH/$LATEST_DIR $DEFAULT_PATH
 else
 	echo "Java JRE is up to date: Version $CURRENT_VERSION Update $CURRENT_UPDATE."
+	if [ ! -e $DEFAULT_PATH ]; then
+		echo "Creating missing symbolic link..."
+		ln -s $JWM_PATH/$CURRENT_DIR $DEFAULT_PATH
+	fi
 fi
 
-if [ `readlink -- "$DEFAULT_PATH"` != $JWM_PATH/$LATEST_DIR ]; then
-	echo "Creating symbolic link..."
-	rm -f $DEFAULT_PATH
-	ln -s $JWM_PATH/$LATEST_DIR $DEFAULT_PATH
-fi
+update-alternatives --install /usr/bin/java java $JWM_PATH/$LATEST_DIR/bin/java $(($LATEST_VERSION*1000+$LATEST_UPDATE))
+#update-alternatives --install /usr/bin/javac javac $JWM_PATH/$LATEST_DIR/bin/javac $(($LATEST_VERSION*100+$LATEST_UPDATE))
