5d73d1ce50b0430afe0c7c3399a2338a5c4917ed
Peter Balling <peter@knowmio.com>
2018-11-03 02:25:00 +0100

    Added function to check for and install dependencies

---
+++ scripts/functions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+ARCHITECTURE=$(dpkg --print-architecture)
+
+# Find and install missing dependencies.
+# Useage: install_depends(STRING)
+#  Where STRING is a string comprised of names of packages spepareted by spaces.
+install_depends() {
+  local dependencies="$1"
+  local missing=""
+  echo "Checking for $(echo "$dependencies" | wc -w) dependencies."
+  # Check for missing dependencies.
+  date +%s:%N
+  for pkg in $dependencies; do
+    dpkg-query -s $pkg:$ARCHITECTURE > /dev/null 2>&1 || missing="$missing $pkg"
+  done
+  date +%s:%N
+  # Install missing packages, if any.
+  if [ ! -z "$missing" ]; then
+    echo "Found $(echo "$missing" | wc -w) missing packages."
+  	apt update -y
+  	apt install -y $missing
+  fi
+}
