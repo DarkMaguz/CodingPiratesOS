c16387fef7525c661ca61e2984e31b541640103c
Peter Balling <peter@knowmio.com>
2019-03-02 23:43:56 +0100

    Updated update_scratch3.sh

--- scripts/update_scratch3.sh
+++ scripts/update_scratch3.sh
@@ -10,15 +10,11 @@ fi
 
 . $PWD/functions.sh
 
-# Install path for firefox.
+# Install path for Scratch 3.
 INSTALL_PATH=/opt/Scratch3
-GIT_URL="https://github.com/LLK/scratch-gui.git"
-
-# Load Gnome desktop entry.
-#DESKTOP_ENTRY="$(cat $DIR/../data/Scratch3.desktop)"
 
 # List of dependencies to be installed.
-DEPEND_PKGS="nodejs"
+DEPEND_PKGS="libgtkmm-3.0-dev autotools-dev libboost-system-dev libboost-thread-dev google-chrome docker git"
 # Check for dependencies.
 install_depends $DEPEND_PKGS
 
@@ -27,12 +23,32 @@ if [ ! -d "$INSTALL_PATH" ]; then
 	mkdir -p $INSTALL_PATH
 fi
 
-# Clone the repo.
-git clone $GIT_URL $INSTALL_PATH
-cd $INSTALL_PATH
+# Get splash screen.
+SPLASH_SCREEN_STATE=none
+if [ ! -d "$INSTALL_PATH/Scratch3-splash" ]; then
+  git clone https://github.com/DarkMaguz/Scratch3-splash.git $INSTALL_PATH/Scratch3-splash
+  BUILD_SPLASH_SCREEN=cloned
+else
+  git -C $INSTALL_PATH/Scratch3-splash remote update &> /dev/null
+  GIT_STATUS=`git -C $INSTALL_PATH/Scratch3-splash status`
+  if [ ! "$GIT_STATUS" =~ "Your branch is up-to-date" ]; then
+    make -C $INSTALL_PATH/Scratch3-splash uninstall
+    git -C $INSTALL_PATH/Scratch3-splash pull --all &> /dev/null
+    BUILD_SPLASH_SCREEN=pulled
+  fi
+fi
 
-npm install
+# Build and install splash screen if needed.
+if [ "$SPLASH_SCREEN_STATE" != "none" ]; then
+  cd $INSTALL_PATH/Scratch3-splash
+  autoreconf -i
+  ./configure --prefix /usr
+  make && make install
+fi
 
 # Add Gnome desktop entry.
-#echo "$DESKTOP_ENTRY"> /usr/share/applications/Scratch3.desktop
-cp $DIR/../data/Scratch3.desktop /usr/share/applications/Scratch3.desktop
+cp $DIR/../data/Scratch3/Scratch3.desktop /usr/share/applications/Scratch3.desktop
+
+# Install scratch3.sh.
+cp $DIR/../data/Scratch3/scratch3.sh $INSTALL_PATH/scratch3.sh
+ln -s $INSTALL_PATH/scratch3.sh /usr/bin/scratch3
