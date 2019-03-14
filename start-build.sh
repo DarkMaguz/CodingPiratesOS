6aa09506b9b15d1c3a16986dc4ada711f79cc583
Peter Balling <peter@knowmio.com>
2019-03-14 11:14:22 +0100

    Removed requirement of root access after adding .dockerignore

--- start-build.sh
+++ start-build.sh
@@ -1,15 +1,9 @@
 #!/bin/sh -e
 
-# Ensure that we have superpowers.
-if [ $(id -u) != 0 ]; then
-        echo "Please run as root"
-        exit
-fi
-
 if [ $(docker ps -a -f "name=/CodingPiratesOS$" -q) ]; then
   docker rm -f CodingPiratesOS
 fi
 
 docker build -t darkmagus/codingpiratesos .
 
-docker run -ti --privileged -v $PWD/build:/usr/app/build -v $PWD/images:/usr/app/images -v $PWD/basics:/usr/app/basics:ro -v $PWD/data:/usr/app/data -v $PWD/scripts:/usr/app/scripts:ro --name="CodingPiratesOS" darkmagus/codingpiratesos
+docker run -ti -u root --privileged -v $PWD/build:/usr/app/build -v $PWD/images:/usr/app/images -v $PWD/basics:/usr/app/basics:ro -v $PWD/data:/usr/app/data -v $PWD/scripts:/usr/app/scripts:ro --name="CodingPiratesOS" darkmagus/codingpiratesos
