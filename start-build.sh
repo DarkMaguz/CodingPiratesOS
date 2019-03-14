b5cda28505d2f804d1ec57edffa90015d5877186
Peter Balling <peter@knowmio.com>
2019-03-14 16:41:16 +0100

    We can't use 'docker run' whith --interactive argument when using ssh

--- start-build.sh
+++ start-build.sh
@@ -6,4 +6,4 @@ fi
 
 docker build -t darkmagus/codingpiratesos .
 
-docker run -ti -u root --privileged -v $PWD/build:/usr/app/build -v $PWD/images:/usr/app/images -v $PWD/basics:/usr/app/basics:ro -v $PWD/data:/usr/app/data -v $PWD/scripts:/usr/app/scripts:ro --name="CodingPiratesOS" darkmagus/codingpiratesos
+docker run -t -u root --privileged -v $PWD/build:/usr/app/build -v $PWD/images:/usr/app/images -v $PWD/basics:/usr/app/basics:ro -v $PWD/data:/usr/app/data -v $PWD/scripts:/usr/app/scripts:ro --name="CodingPiratesOS" darkmagus/codingpiratesos
