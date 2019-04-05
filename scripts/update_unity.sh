e2a29a31c7ba6855b25fadbcd699f08faf44b2e5
Peter Balling <peter.balling@gmail.com>
2019-04-05 22:13:58 +0200

    Added missing dependency package jshon

--- scripts/update_unity.sh
+++ scripts/update_unity.sh
@@ -8,7 +8,7 @@ DIR=$(dirname `realpath $0`)
 # Install path for Unity.
 UNITY_PATH=/opt/Unity
 
-DEPEND_PKGS="curl wget libxml2-utils xdg-utils coreutils gconf-service lib32gcc1 lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libglib2.0-0 libglu1-mesa libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g"
+DEPEND_PKGS="curl wget libxml2-utils xdg-utils jshon coreutils gconf-service lib32gcc1 lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libglib2.0-0 libglu1-mesa libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g"
 
 # Ensure that we have superpowers.
 if [ $(id -u) != 0 ]; then
