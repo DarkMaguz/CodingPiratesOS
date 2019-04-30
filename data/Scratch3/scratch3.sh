4bcec463428f76886a55cef84c24c7f8c17ace4f
Peter Balling <peter.balling@gmail.com>
2019-04-30 16:00:35 +0200

    Improved scratch3 scripts

--- data/Scratch3/scratch3.sh
+++ data/Scratch3/scratch3.sh
@@ -1,13 +1,9 @@
 #!/bin/sh -e
 
-if [ $(docker ps -a -f "name=/scratch3$" -q) ]; then
-  docker rm -f scratch3
-fi
+docker rm -f scratch3 || :
 
 docker pull darkmagus/scratch3
-docker run -p 8601:8601 -d --name scratch3 darkmagus/scratch3
+docker run -p 8601:80 -d --name scratch3 darkmagus/scratch3
 
-s3splash
-
-#wget http://localhost:8601/ -t 10 --waitretry 1 -o /dev/null
+wget http://localhost:8601/ -t 10 --waitretry 1 -o /dev/null
 google-chrome --new-window --app http://localhost:8601/
