5c2cf1d977091193fa59d877652f6506b1709ab1
Peter Balling <peter.balling@gmail.com>
2019-05-27 11:07:20 +0200

    Added cleanup script for Jenkins

---
+++ jenkins-cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+if [ $(docker ps -a -f "name=/CodingPiratesOS$1$" -q) ]; then
+  docker rm -f CodingPiratesOS$1
+fi
+
+docker build -t darkmagus/codingpiratesos .
+
+docker run -t \
+  -u root \
+  -v $PWD/build:/usr/app/build \
+  --privileged \
+  -v $PWD/images:/usr/app/images \
+  -v $PWD/basics:/usr/app/basics:ro \
+  -v $PWD/data:/usr/app/data \
+  -v $PWD/scripts:/usr/app/scripts:ro \
+  --name="CodingPiratesOS$1" \
+  darkmagus/codingpiratesos \
+  rm -f /usr/app/images/*
