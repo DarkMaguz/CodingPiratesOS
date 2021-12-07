d34e22c15bb3022dfb37f875c9879bf3a2cb9227
Peter <peter.balling@gmail.com>
2021-12-07 20:48:41 +0100

    Finished apt archive test

--- test/apt/run-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-for file in ../../basics/config/archives/*list; do
-  echo $file
-
-done
--- test/apt/test-archives.py
+++ test/apt/test-archives.py
@@ -1,5 +1,15 @@
 import os
 import docker
+import multiprocessing as mp
+
+verbose = True
+archivesPath = os.path.abspath('../../basics/config/archives/')
+
+def buildTestImage():
+  client = docker.from_env()
+  with open('Dockerfile', 'rb') as df:
+    client.images.build(fileobj=df, tag='darkmagus/apt-test')
+
 
 def runDocker(listFile, keyFile):
   client = docker.from_env()
@@ -20,30 +30,52 @@ def runDocker(listFile, keyFile):
   try:
     container = client.containers.run('darkmagus/apt-test', command='./docker-run.sh', volumes=volumes, detach=True)
     container.wait()
-    #print('kkk')
-    print(container.logs())
+    logs = container.logs().decode()
+    if 'Err:' in logs:
+      return logs
   except docker.errors.ContainerError as e:
-    print('Error': e)
-    raise e
+    return str(e)
   else:
     return None
 
 
-def buildImage():
-  client = docker.from_env()
-  with open('Dockerfile', 'rb') as df:
-    client.images.build(fileobj=df, tag='darkmagus/apt-test')
+def runTest(repoName, listFile, keyFile, queue):
+  assert os.path.exists(keyFile)
+  queue.put([repoName, runDocker(listFile, keyFile)])
 
-buildImage()
 
-archivesPath = os.path.abspath('../../basics/config/archives/')
-for file in os.listdir(archivesPath):
-  if file.endswith('list'):
-    listFile = os.path.join(archivesPath, file)
-    tmp = file.removesuffix('.list') + '.key'
-    keyFile = os.path.join(archivesPath, tmp)
-    #print('key:', keyFile)
-    #print('file:', file)
-    assert os.path.exists(keyFile)
-    assert (runDocker(listFile, keyFile) == None)
-    break
+def joinProcesses(processes):
+  for p in processes:
+    p.join()
+
+
+def printFailure(repo):
+  if not verbose: return
+  lines = str(repo[1]).split('\n')
+  print('%s failed:\n\t%s' % (repo[0], '\n\t'.join(lines)))
+
+
+def hasFailedRepo(queue):
+  aRepoHasFailed = False
+  while not queue.empty():
+    result = queue.get()
+    if result[1] != None:
+      printFailure(result)
+      aRepoHasFailed = True
+  return aRepoHasFailed
+
+
+if __name__ == '__main__':
+  buildTestImage()
+  processes = []
+  queue = mp.Queue()
+  for file in os.listdir(archivesPath):
+    if file.endswith('list'):
+      repoName = file.removesuffix('.list')
+      listFile = os.path.join(archivesPath, file)
+      keyFile = os.path.join(archivesPath, repoName + '.key')
+      p = mp.Process(target=runTest, args=(repoName, listFile, keyFile, queue))
+      p.start()
+      processes.append(p)
+  joinProcesses(processes)
+  assert not hasFailedRepo(queue)
