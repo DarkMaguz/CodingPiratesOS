4442318858aace4a053f70532c913a8e86404f64
Peter <peter.balling@gmail.com>
2021-12-21 23:47:58 +0100

    Applied temporary fix for slack

--- test/apt/docker-run-pkg-list.sh
+++ test/apt/docker-run-pkg-list.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+wget http://ftp.dk.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_amd64.deb
+wget http://ftp.dk.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-4_amd64.deb
+dpkg -i libappindicator3-1_0.4.92-7_amd64.deb libindicator3-7_0.5.0-4_amd64.deb || true
+apt-get install -f -y
+
 echo "$KEYS" | tr ',' '\n' | while read key; do
   apt-key add $key
 done
