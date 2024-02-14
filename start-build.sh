49df71540108551f419af176bb80801dc3c181b4
Peter Balling <peter.balling@gmail.com>
2024-02-14 16:42:59 +0100

    Updated start-build.sh

--- start-build.sh
+++ start-build.sh
@@ -10,19 +10,26 @@ if [ $(docker ps -a -f "name=/CodingPiratesOS$1$" -q) ]; then
   docker rm -f CodingPiratesOS$1
 fi
 
-# Create a bridge network if it doesn't already exist.
-if [ -z "$(docker network ls | grep cpos)" ]; then
-  docker network create --driver bridge cpos
+# Specify the network to use.
+DOCKER_NETWORK=default
+
+# If the network "cpos" exists, use it. 
+if [ -n "$(docker network ls | grep cpos)" ]; then
+  DOCKER_NETWORK=cpos
 fi
 
+# Build the docker image.
 docker build \
-  --network=cpos \
+  --network $DOCKER_NETWORK \
   -t darkmagus/codingpiratesos .
 
+# Start building the live cpos iso.
+# It is necessary to run the container with --privileged in order 
+# to mounting /dev/pts and /proc whith chroot inside the container.
+# See https://github.com/docker/docker/issues/11896
 docker run -ti \
-  -u root \
+  --network $DOCKER_NETWORK \
   --privileged \
-  --network=cpos \
   -v $PWD/build:/usr/app/build:rw \
   -v $PWD/images:/usr/app/images \
   -v $PWD/basics:/usr/app/basics:ro \
